1e8f2cff3d68e95f7fe495ee9a44811ebdab683c
Ângelo Antunes da Rocha <angeloantunesdarocha@gmail.com>
2026-07-25 09:10:13 -0300

    feat: adicionar service worker offline com atualização segura

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_VERSION = 'scanmaster-v2-2026-07-25';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './scanmaster-icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_VERSION).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_VERSION).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          void caches.open(CACHE_VERSION).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request).then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          void caches.open(CACHE_VERSION).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      });
+      return cached || network;
+    })
+  );
+});
